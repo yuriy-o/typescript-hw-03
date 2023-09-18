20d702b2db76ca17928d996661eb85079fd8db29
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-09-18 22:35:30 +0300

    les 4

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,54 @@
+class Key {
+  constructor(private signature: number = Math.random()) {}
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  private tenants: string[] = [];
+  door: boolean;
+  key: Key;
+
+  constructor(door: boolean, key: Key) {
+    this.door = door;
+    this.key = key;
+  }
+
+  comeIn(person: string) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (!this.door && key === this.key) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
-const house = new MyHouse(key);
+const house = new MyHouse(true, key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
+//! тут помилка → Аргумент типу Person не може бути присвоєний параметру типу string
 house.comeIn(person);
-
 
 export {};
